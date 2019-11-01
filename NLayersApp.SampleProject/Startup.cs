f93fb458b0f956e59fcda6ad6e4253c4f96a05b9
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-01 09:11:10 +0100

    removing IdentityServer dependencies, references and startup configuration

--- NLayersApp.SampleProject/Startup.cs
+++ NLayersApp.SampleProject/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,17 +13,14 @@ using NLayersApp.CQRS.DependencyInjection;
 using NLayersApp.Persistence.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using IdentityServer4.Services;
 using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
-using IdentityServer4.EntityFramework.Interfaces;
-using IdentityServer4.EntityFramework.Entities;
+using OpenIddict.Abstractions;
+using System.Reflection;
+using OpenIddict.Core;
 using System.Threading.Tasks;
-using System.Threading;
-using NLayersApp.Extensions;
-using NLayersApp.Authorisation.Models;
-using NLayersApp.Authorisation;
-using NLayersApp.Authorization.Extensions;
+using OpenIddict.EntityFrameworkCore.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using NLayersApp.Controllers;
 
 namespace NLayersApp.SampleProject
@@ -44,20 +42,18 @@ namespace NLayersApp.SampleProject
             services.AddScoped<ITypesResolver>(s => resolver);
             // services.AddScoped<TDbContext>();
 
-            services.AddDbContext<IContext, TDbContextConfigStore>(optionsAction: (s, o) =>
+            services.AddDbContext<IContext, TDbContext>(optionsAction: (s, o) =>
             {
                 o.UseSqlServer("Server=.\\;Initial Catalog=nlayersapp-tests; Integrated Security=True;");
             }, ServiceLifetime.Scoped);
 
-            services.AddAuthorizationConfig<TDbContextConfigStore, AppUser, IdentityRole, string>();
             // ConfigureAuthenticationAndAuthorisation(services);
 
             services.AddMediatRHandlers(resolver);
 
             services.AddControllers()
                 .UseDynamicControllers(resolver)
-                .AddControllersAsServices()
-                .UseNLayersAppAccountController();
+                .AddControllersAsServices();
         }
 
         private void ConfigureAuthenticationAndAuthorisation(IServiceCollection services)
@@ -72,46 +68,16 @@ namespace NLayersApp.SampleProject
             .AddCookie();
 
 
-            services.AddIdentity<AppUser, IdentityRole>()
+            services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<TDbContextConfigStore>()
-                .AddUserManager<UserManager<AppUser>>()
+                .AddUserManager<UserManager<IdentityUser>>()
                 .AddRoleManager<RoleManager<IdentityRole>>()
                 .AddDefaultTokenProviders();
             //.AddClaimsPrincipalFactory();
 
-            var builder = services.AddIdentityServer()
-                .AddConfigurationStore<TDbContextConfigStore>()
-                .AddAspNetIdentity<AppUser>()
-                .AddInMemoryApiResources(AuthConfig.Apis)
-                .AddInMemoryClients(AuthConfig.Clients)
-                .AddOperationalStore(options =>
-                {
-                    options.ConfigureDbContext = builder => builder.UseSqlServer("Server=.\\;Initial Catalog=nlayersapp-tests; Integrated Security=True;");
-                    // this enables automatic token cleanup. this is optional.
-                    options.EnableTokenCleanup = true;
-                    options.TokenCleanupInterval = 30; // interval in seconds
-                })
-                //.AddInMemoryPersistedGrants()
-                .AddInMemoryIdentityResources(AuthConfig.GetIdentityResources())
-                .AddInMemoryApiResources(AuthConfig.GetApiResources())
-                .AddInMemoryClients(AuthConfig.GetClients());
-
-            /* We'll play with this down the road... 
-                services.AddAuthentication()
-                .AddGoogle("Google", options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    options.ClientId = "<insert here>";
-                    options.ClientSecret = "<insert here>";
-                });*/
-
-            services.AddTransient<IProfileService, IdentityClaimsProfileService>();
-
             services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
                .AllowAnyMethod()
                .AllowAnyHeader())); ;
-
-            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -132,7 +98,7 @@ namespace NLayersApp.SampleProject
                     var error = context.Features.Get<IExceptionHandlerFeature>();
                     if (error != null)
                     {
-                        context.Response.AddApplicationError(error.Error.Message);
+                        // context.Response.AddApplicationError(error.Error.Message);
                         await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
                     }
                 });
@@ -140,7 +106,6 @@ namespace NLayersApp.SampleProject
 
             app.UseCors("AllowAll");
             app.UseHttpsRedirection();
-            app.UseIdentityServer().UseAuthentication();
             
             //app.UseAuthentication();
 
