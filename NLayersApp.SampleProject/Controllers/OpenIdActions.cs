0754697d4b2637ddd7dc29a5e037e0a6c9363dfa
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2019-11-01 10:54:11 +0100

    adding authorize/token/userinfo endpoints

---
+++ NLayersApp.SampleProject/Controllers/OpenIdActions.cs
@@ -0,0 +1,69 @@
+﻿using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OpenIdConnect.Server;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OpenIddict.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NLayersApp.SampleProject.Controllers
+{
+
+    public class OpenIdActions: ControllerBase
+    {
+        [HttpPost("connect/authorize")]
+        public async Task<IActionResult> Authorize(OpenIdConnectRequest request)
+        {
+            // Create a new ClaimsPrincipal containing the claims that
+            // will be used to create an id_token, a token or a code.
+            var claims = new List<Claim>();
+            claims.Add(new Claim(OpenIdConnectConstants.Claims.Username, request.Username));
+            var identity = new ClaimsIdentity(claims, "OpenIddict");
+            var principal = new ClaimsPrincipal(identity);
+
+            // Create a new authentication ticket holding the user identity.
+            var ticket = new AuthenticationTicket(principal,
+                new AuthenticationProperties(),
+                OpenIdConnectServerDefaults.AuthenticationScheme);
+
+            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+            return SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
+        }
+
+        [HttpGet("connect/userinfo"), Authorize(AuthenticationSchemes = OpenIddictServerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> UserInfo()
+        {
+            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+            return new OkObjectResult(User);// (ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
+        }
+        [HttpPost("connect/token")]
+        public async Task<IActionResult> Token(OpenIdConnectRequest request)
+        {
+            // Create a new ClaimsPrincipal containing the claims that
+            // will be used to create an id_token, a token or a code.
+            var claims = new List<Claim>();
+            claims.Add(new Claim(OpenIdConnectConstants.Claims.Subject, "user-0001"));
+            var identity = new ClaimsIdentity(claims, "OpenIddict");
+            var principal = new ClaimsPrincipal(identity);
+
+            // Create a new authentication ticket holding the user identity.
+            var ticket = new AuthenticationTicket(principal,
+                new AuthenticationProperties(),
+                OpenIdConnectServerDefaults.AuthenticationScheme);
+
+            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+            return SignIn(ticket.Principal, ticket.Properties, ticket.AuthenticationScheme);
+        }
+    }
+}
--- NLayersApp.SampleProject/Startup.cs
+++ NLayersApp.SampleProject/Startup.cs
@@ -152,7 +152,6 @@ namespace NLayersApp.SampleProject
             app.UseAuthentication();
 
             // app.UseClientSideBlazorFiles<Client.Startup>();
-            
             app.UseRouting();
 
             app.UseAuthorization();
@@ -162,5 +161,74 @@ namespace NLayersApp.SampleProject
                 endpoints.MapControllers();
             });
         }
+
+        private async Task InitializeAsync(IServiceProvider services)
+        {
+            // Create a new service scope to ensure the database context is correctly disposed when this methods returns.
+            using var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+            await context.Database.EnsureCreatedAsync();
+
+            var manager = scope.ServiceProvider.GetRequiredService<OpenIddictApplicationManager<OpenIddictApplication>>();
+
+            if (await manager.FindByClientIdAsync("mvc") == null)
+            {
+                var descriptor = new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "mvc",
+                    ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654",
+                    DisplayName = "MVC client application",
+                    PostLogoutRedirectUris = { new Uri("http://localhost:53507/signout-callback-oidc") },
+                    RedirectUris = { new Uri("http://localhost:53507/signin-oidc") },
+                    Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Authorization,
+                        OpenIddictConstants.Permissions.Endpoints.Logout,
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                        OpenIddictConstants.Permissions.GrantTypes.RefreshToken,
+                        OpenIddictConstants.Permissions.Scopes.Email,
+                        OpenIddictConstants.Permissions.Scopes.Profile,
+                        OpenIddictConstants.Permissions.Scopes.Roles
+                    }
+                };
+
+                await manager.CreateAsync(descriptor);
+            }
+
+            // To test this sample with Postman, use the following settings:
+            //
+            // * Authorization URL: http://localhost:54540/connect/authorize
+            // * Access token URL: http://localhost:54540/connect/token
+            // * Client ID: postman
+            // * Client secret: [blank] (not used with public clients)
+            // * Scope: openid email profile roles
+            // * Grant type: authorization code
+            // * Request access token locally: yes
+            if (await manager.FindByClientIdAsync("postman") == null)
+            {
+                var descriptor = new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "postman",
+                    DisplayName = "Postman",
+                    ClientSecret = "simple_secret",
+                    RedirectUris = { new Uri("https://app.getpostman.com/oauth2/callback") },
+                    Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Authorization,
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                        OpenIddictConstants.Permissions.GrantTypes.Password,
+                        OpenIddictConstants.Permissions.Scopes.Email,
+                        OpenIddictConstants.Permissions.Scopes.Profile,
+                        OpenIddictConstants.Permissions.Scopes.Roles
+                    }
+                };
+
+                await manager.CreateAsync(descriptor);
+            }
+        }
+
     }
 }
